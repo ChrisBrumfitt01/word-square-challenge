fb89550e1132731732f0dabc8ba6ec51ef9af539
ChrisBrumfitt01 <chrisbrumfitt@hotmail.co.uk>
2023-02-08 23:51:31 +0000

    Add some tests and a bit of refactoring

--- src/main/java/com/naimuri/wordsquare/WordSquareChallenge/fileio/DictionaryParser.java
+++ src/main/java/com/naimuri/wordsquare/WordSquareChallenge/fileio/DictionaryParser.java
@@ -2,16 +2,12 @@ package com.naimuri.wordsquare.WordSquareChallenge.fileio;
 
 import com.naimuri.wordsquare.WordSquareChallenge.model.Dictionary;
 import com.naimuri.wordsquare.WordSquareChallenge.util.WordUtil;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Component
 public class DictionaryParser {
@@ -27,18 +23,14 @@ public class DictionaryParser {
 
     public Dictionary getDictionary(int size, String text) {
         String url = "http://norvig.com/ngrams/enable1.txt";
-        return restTemplate.execute(url, HttpMethod.GET, null, response -> {
-            BufferedReader br = new BufferedReader(new InputStreamReader(response.getBody()));
-            List<String> words = new ArrayList<>();
-            String line;
-            while((line = br.readLine()) != null){
-                if (isMatch(line, size, text)){
-                    words.add(line.toLowerCase());
-                }
-            }
-            br.close();
-            return new Dictionary(words);
-        });
+        String result = restTemplate.getForObject(url, String.class);
+        String[] words = result.split("\n");
+        return new Dictionary(
+                Arrays.stream(words)
+                .filter(w -> isMatch(w, size, text))
+                .map(String::toLowerCase)
+                .collect(Collectors.toList())
+        );
     }
 
     private boolean isMatch(String word, int size, String text) {
--- src/test/java/com/naimuri/wordsquare/WordSquareChallenge/fileio/DictionaryParserTest.java
+++ src/test/java/com/naimuri/wordsquare/WordSquareChallenge/fileio/DictionaryParserTest.java
@@ -1,79 +1,68 @@
-//package com.naimuri.wordsquare.WordSquareChallenge.fileio;
-//
-//import com.naimuri.wordsquare.WordSquareChallenge.util.WordUtil;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnitRunner;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.mockito.stubbing.Answer;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.web.client.ResponseExtractor;
-//import org.springframework.web.client.RestTemplate;
-//
-//import java.io.BufferedReader;
-//import java.io.InputStreamReader;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.mockito.ArgumentMatchers.*;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//@ExtendWith(MockitoExtension.class)
-//public class DictionaryParserTest {
-//
-//    private static final String URL = "http://norvig.com/ngrams/enable1.txt";
-//
-//    @Mock
-//    private RestTemplate restTemplate;
-//
-//    @Mock
-//    private WordUtil wordUtil;
-//
-//    @InjectMocks
-//    private DictionaryParser dictionaryParser;
-//
-//    @Test
-//    public void testGetDictionary() {
-//        List<String> words = new ArrayList<>();
-//        words.add("test");
-//        words.add("word");
-//
-//        when(restTemplate.execute(anyString(), any(HttpMethod.class), any(), any()))
-//                .thenAnswer(new Answer<List<String>>() {
-//
-//                });
-//
-//        when(mockWordUtil.isAnagram(anyString(), anyString()))
-//                .thenReturn(true);
-//
-//        Dictionary result = parser.getDictionary(4, "test");
-//        assertEquals(2, result.getWords().size());
-//        assertTrue(result.getWords().containsAll(words));
-//    }
-//
-////    @Test
-////    public void getDictionary_shouldMakeACallToExpectedUrl() {
-////        when(restTemplate.execute(anyString(), any(HttpMethod.class), eq(null), any(ResponseExtractor.class))).thenReturn(List.of());
-////        dictionaryParser.getDictionary(1, "a");
-////        verify(restTemplate).execute(eq(URL), eq(HttpMethod.GET), eq(null), any(ResponseExtractor.class));
-////    }
-////
-////    @Test
-////    public void isValidWord_ReturnsTrueWhenWordExists() {
-////        when(restTemplate.execute(anyString(), any(HttpMethod.class), eq(null), any(ResponseExtractor.class))).thenReturn(List.of("word"));
-////        dictionaryParser.afterPropertiesSet();
-////        assertThat(dictionaryParser.isValidWord("word")).isTrue();
-////    }
-////
-////    @Test
-////    public void isValidWord_ReturnsFalseWhenWordDoesNotExist() {
-////        when(restTemplate.execute(anyString(), any(HttpMethod.class), eq(null), any(ResponseExtractor.class))).thenReturn(List.of("word"));
-////        dictionaryParser.afterPropertiesSet();
-////        assertThat(dictionaryParser.isValidWord("hello")).isFalse();
-////    }
-//}
+package com.naimuri.wordsquare.WordSquareChallenge.fileio;
+
+import com.naimuri.wordsquare.WordSquareChallenge.model.Dictionary;
+import com.naimuri.wordsquare.WordSquareChallenge.util.WordUtil;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.RestTemplate;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class DictionaryParserTest {
+
+    private static final String URL = "http://norvig.com/ngrams/enable1.txt";
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @Mock
+    private WordUtil wordUtil;
+
+    @InjectMocks
+    private DictionaryParser dictionaryParser;
+
+    @Test
+    public void getDictionary_shouldMakeCallToExpectedUrl() {
+        when(restTemplate.getForObject(URL, String.class)).thenReturn("test");
+        dictionaryParser.getDictionary(4, "test");
+        verify(restTemplate).getForObject(URL, String.class);
+    }
+
+    @Test
+    public void getDictionary_shouldFilterOnWordLength(){
+        String response = "word\ntoo\nnot\nrose";
+        when(restTemplate.getForObject(URL, String.class)).thenReturn(response);
+        when(wordUtil.isAnagram(any(), any())).thenReturn(true);
+        Dictionary actual = dictionaryParser.getDictionary(4, "test");
+        assertThat(actual.getWords()).containsExactly("word", "rose");
+    }
+
+    @Test
+    public void getDictionary_shouldFilterOnResponseFromIsAnagram(){
+        String letters = "abc";
+        String response = "word\nrose";
+        when(restTemplate.getForObject(URL, String.class)).thenReturn(response);
+        when(wordUtil.isAnagram("word", letters)).thenReturn(true);
+        when(wordUtil.isAnagram("rose", letters)).thenReturn(false);
+        Dictionary actual = dictionaryParser.getDictionary(4, letters);
+        assertThat(actual.getWords()).containsExactly("word");
+    }
+
+    @Test
+    public void getDictionary_shouldConvertWordsToLowerCase(){
+        String response = "Word\nROSE";
+        when(restTemplate.getForObject(URL, String.class)).thenReturn(response);
+        when(wordUtil.isAnagram(any(), any())).thenReturn(true);
+        Dictionary actual = dictionaryParser.getDictionary(4, "test");
+        assertThat(actual.getWords()).containsExactly("word", "rose");
+    }
+
+}
