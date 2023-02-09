187e5ad0909c916f320d2fffdedaefa2dd172cf8
ChrisBrumfitt01 <chrisbrumfitt@hotmail.co.uk>
2023-02-09 17:41:25 +0000

    Delete UI

--- src/main/java/com/naimuri/wordsquare/WordSquareChallenge/WordSquareChallengeApplication.java
+++ src/main/java/com/naimuri/wordsquare/WordSquareChallenge/WordSquareChallengeApplication.java
@@ -1,14 +1,12 @@
 package com.naimuri.wordsquare.WordSquareChallenge;
 
 import com.naimuri.wordsquare.WordSquareChallenge.services.WordSquareService;
-import com.naimuri.wordsquare.WordSquareChallenge.view.UI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
 
 @SpringBootApplication
 public class WordSquareChallengeApplication implements CommandLineRunner {
